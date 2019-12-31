8e3b49ae21d3804aab713c46e1c3841d4305adfd
AM-Myrick <amyrick.820@gmail.com>
2019-12-31 17:59:23 -0500

    fix: switch to function component, state unneeded

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,40 +5,31 @@ import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import { Switch, Route } from 'react-router-dom';
 
-export default class App extends Component {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      isCurrentMode: false,
-      isMatMode: false,
-      isTimed: false,
-    };
-  }
-
-  render() {
-    return (
-      <Box>
-        <Header />
-        <Container>
-          <Switch>
-            <Route exact path='/' 
-              render={props => (
-                <NameGame {...props} gameMode='all' />
-              )}
-            />
-            <Route path='/current-employees' 
-              render={props => (
-                <NameGame {...props} gameMode='current' />
-              )}
-            />
-            <Route path='/mat-employees' 
-              render={props => (
-                <NameGame {...props} gameMode='mat' />
-              )}
-            />
-          </Switch>
-        </Container>
-      </Box>
-    );
-  }
+const App: React.FC = (props) => {
+  return (
+    <Box>
+      <Header />
+      <Container>
+        <Switch>
+          <Route exact path='/' 
+            render={props => (
+              <NameGame {...props} gameMode='all' />
+            )}
+          />
+          <Route path='/current-employees' 
+            render={props => (
+              <NameGame {...props} gameMode='current' />
+            )}
+          />
+          <Route path='/mat-employees' 
+            render={props => (
+              <NameGame {...props} gameMode='mat' />
+            )}
+          />
+        </Switch>
+      </Container>
+    </Box>
+  );
 }
+
+export default App;
