da5d725fae72b4554c6681170c9d1316a5b3995b
AM-Myrick <amyrick.820@gmail.com>
2020-01-05 17:13:42 -0500

    fix: correct infinite loop bug on back-end

--- back-end/src/Helpers/helperFunctions.ts
+++ back-end/src/Helpers/helperFunctions.ts
@@ -1,12 +1,25 @@
 import axios from "axios";
 import Employee from "../Models/Employee";
 
+// shuffle function for use when numOfOptions > length of requested employees
+const shuffleArray = (array: Employee[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 // access api and get all employees
 export const getAllEmployees = async () => {
   try {
     const allEmployees: Employee[] = [];
-    const response = await axios.get("https://willowtreeapps.com/api/v1.0/profiles/");
-    response.data = response.data.filter((employee: Employee) => employee.headshot.hasOwnProperty("url"));
+    const response = await axios.get(
+      "https://willowtreeapps.com/api/v1.0/profiles/"
+    );
+    response.data = response.data.filter((employee: Employee) =>
+      employee.headshot.hasOwnProperty("url")
+    );
     allEmployees.push(...response.data);
     return allEmployees;
   } catch (err) {
@@ -18,7 +31,10 @@ export const getAllEmployees = async () => {
 
 // return array of employees named Matt or Matthew
 export const getMatEmployees = (employees: Employee[]) => {
-  return employees.filter((employee) => employee.firstName === "Matthew" || employee.firstName === "Matt");
+  return employees.filter(
+    (employee) =>
+      employee.firstName === "Matthew" || employee.firstName === "Matt"
+  );
 };
 
 // return array of current employees
@@ -27,13 +43,20 @@ export const getCurrentEmployees = (employees: Employee[]) => {
 };
 
 // return array of random employees
-export const getRandomEmployees = (numOfOptions: number, employees: Employee[]) => {
+export const getRandomEmployees = (
+  numOfOptions: number,
+  employees: Employee[]
+) => {
+  if (numOfOptions > employees.length) {
+    return shuffleArray(employees);
+  }
   // using a set to ensure no duplicate results
   const selectedEmployees: Set<Employee> = new Set();
 
   while (selectedEmployees.size < numOfOptions) {
     // store a random employee
-    const randomEmployee: Employee = employees[Math.floor(Math.random() * employees.length)];
+    const randomEmployee: Employee =
+      employees[Math.floor(Math.random() * employees.length)];
 
     selectedEmployees.add(randomEmployee);
   }
@@ -41,7 +64,8 @@ export const getRandomEmployees = (numOfOptions: number, employees: Employee[]) 
 };
 
 export const getAnswer = (employees: Employee[]) => {
-  const selectedEmployee: Employee = employees[Math.floor(Math.random() * employees.length)];
+  const selectedEmployee: Employee =
+    employees[Math.floor(Math.random() * employees.length)];
   return `${selectedEmployee.firstName} ${selectedEmployee.lastName}`;
 };
 
@@ -49,6 +73,10 @@ export const sendError = (res: any, err: any, message: string) => {
   res.status(404).json({ err, message });
 };
 
-export const sendSuccess = (res: any, selectedEmployees: Employee[], answer: string) => {
+export const sendSuccess = (
+  res: any,
+  selectedEmployees: Employee[],
+  answer: string
+) => {
   res.status(200).json({ selectedEmployees, answer });
 };
